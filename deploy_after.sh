411a53dd2da5d76f7d2292d8296676d168978022
htchepannou <htchepannou@expedia.com>
2016-12-17 15:22:21 -0500

    integration with kribi

--- deploy_after.sh
+++ deploy_after.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then
-  echo "GET http://kribi.tchepannou.io/v1/application/kribi/init_artifact?version=$TRAVIS_COMMIT"
   curl -H "api_key: $KRIBI_API_KEY" http://kribi.tchepannou.io/v1/application/kribi/init_artifact?version=$TRAVIS_COMMIT
-
-  echo
-  echo "GET http://kribi.tchepannou.io/v1/application/kribi/deploy?name=kribi&environment=PROD&region=us-east-1&undeployOld=true&version=$TRAVIS_COMMIT"
   curl -H "api_key: $KRIBI_API_KEY" http://kribi.tchepannou.io/v1/application/kribi/deploy?name=kiosk-pipeline&environment=PROD&region=us-east-1&undeployOld=true&version=$TRAVIS_COMMIT
   exit $?
 

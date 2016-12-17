1e0dd6a90518879a1cf4f7ac6147f69956380026
htchepannou <htchepannou@expedia.com>
2016-12-17 18:03:41 -0500

    fix deployment

--- deploy_after.sh
+++ deploy_after.sh
@@ -2,7 +2,10 @@
 
 APP="kiosk-pipeline"
 if [ "$TRAVIS_BRANCH" == "master" ]; then
+  echo "curl http://kribi.tchepannou.io/v1/application/$APP/init_artifact?version=$TRAVIS_COMMIT"
   curl -H "api_key: $KRIBI_API_KEY" http://kribi.tchepannou.io/v1/application/$APP/init_artifact?version=$TRAVIS_COMMIT
+
+  echo "curl http://kribi.tchepannou.io/v1/application/$APP/deploy?name=$APP&environment=PROD&region=us-east-1&undeployOld=true&version=$TRAVIS_COMMIT"
   curl -H "api_key: $KRIBI_API_KEY" http://kribi.tchepannou.io/v1/application/$APP/deploy?name=$APP&environment=PROD&region=us-east-1&undeployOld=true&version=$TRAVIS_COMMIT
   exit $?
 

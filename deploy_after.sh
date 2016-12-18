30a80d8dd7f05f5b65f50b37c91f991e8d81df34
htchepannou <htchepannou@expedia.com>
2016-12-17 22:57:04 -0500

    deployment

--- deploy_after.sh
+++ deploy_after.sh
@@ -2,11 +2,13 @@
 
 APP="kiosk-pipeline"
 if [ "$TRAVIS_BRANCH" == "master" ]; then
-  echo "curl http://kribi.tchepannou.io/v1/application/$APP/init_artifact?version=$TRAVIS_COMMIT"
-  curl -H "api_key: $KRIBI_API_KEY" http://kribi.tchepannou.io/v1/application/$APP/init_artifact?version=$TRAVIS_COMMIT
+  ARTIFACT_URL="http://kribi.tchepannou.io/v1/application/$APP/init_artifact?version=$TRAVIS_COMMIT"
+  curl -v -H "api_key: $KRIBI_API_KEY" $ARTIFACT_URL
 
-  echo "curl http://kribi.tchepannou.io/v1/application/$APP/deploy?name=$APP&environment=PROD&region=us-east-1&undeployOld=true&version=$TRAVIS_COMMIT"
-  curl -H "api_key: $KRIBI_API_KEY" http://kribi.tchepannou.io/v1/application/$APP/deploy?name=$APP&environment=PROD&region=us-east-1&undeployOld=true&version=$TRAVIS_COMMIT
+  echo
+  echo
+  DEPLOY_URL="http://kribi.tchepannou.io/v1/application/$APP/deploy?environment=PROD&region=us-east-1&release=true&undeployOld=true&version=$TRAVIS_COMMIT"
+  curl -v -H "api_key: $KRIBI_API_KEY" -m 600 --connect-timeout 120 $DEPLOY_URL
   exit $?
 
 fi

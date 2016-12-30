0696241c3332cac6391b50d0c53d5684c23981ca
htchepannou <htchepannou@expedia.com>
2016-12-29 23:51:08 -0500

    add new feeds

---
+++ src/main/resources/db/migration/V1_6__onboard_new_feeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO feed (id, name, url, logo_url, path) VALUE (8, 'CamerPost', 'http://www.camerpost.com', 'feeds/camerpost.png', NULL);
+INSERT INTO feed (id, name, url, logo_url, path) VALUE (8, '237Online', 'http://www.237online.com', 'feeds/237online.png', NULL);

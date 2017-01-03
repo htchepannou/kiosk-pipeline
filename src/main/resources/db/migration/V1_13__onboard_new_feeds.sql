1fd9097c576124a545be0f7b4987bbafcbdc7951
Herve Tchepannou <herve.tchepannou@gmail.com>
2017-01-03 00:33:36 -0500

    fix sql script

--- src/main/resources/db/migration/V1_13__onboard_new_feeds.sql
+++ src/main/resources/db/migration/V1_13__onboard_new_feeds.sql
@@ -1,4 +1,4 @@
-UPDATE FEED set url='http://www.mbokotv.com' WHERE id=14;
+UPDATE feed set url='http://www.mbokotv.com' WHERE id=14;
 
 INSERT INTO feed (id, name, url, logo_url, path) VALUE (15, 'Cameroon Online', 'http://www.cameroononline.org', 'feeds/cameroon-online.png', null);
 INSERT INTO feed (id, name, url, logo_url, path) VALUE (16, 'Aucun Lait', 'http://www.aucunlait.net', 'feeds/aucun-lait.png', null);

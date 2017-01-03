f20c3c06bf000d5ffb288b117a1b1345cf83602d
Herve Tchepannou <herve.tchepannou@gmail.com>
2017-01-03 00:20:36 -0500

    add new feeds

---
+++ src/main/resources/db/migration/V1_13__onboard_new_feeds.sql
@@ -0,0 +1,6 @@
+UPDATE FEED set url='http://www.mbokotv.com' WHERE id=14;
+
+INSERT INTO feed (id, name, url, logo_url, path) VALUE (15, 'Cameroon Online', 'http://www.cameroononline.org', 'feeds/cameroon-online.png', null);
+INSERT INTO feed (id, name, url, logo_url, path) VALUE (16, 'Aucun Lait', 'http://www.aucunlait.net', 'feeds/aucun-lait.png', null);
+INSERT INTO feed (id, name, url, logo_url, path) VALUE (17, 'Koalo Mag', 'http://www.kaolomag.com', 'feeds/kaolo-mag.jpg', '*.html');
+INSERT INTO feed (id, name, url, logo_url, path) VALUE (18, 'The Hot Jem', 'http://www.thehotjem.com', 'feeds/the-hot-jem.jpg', null);

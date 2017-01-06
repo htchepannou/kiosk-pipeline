7c39d751cc09c7bb44d0d451f73714d5d8e49247
htchepannou <htchepannou@expedia.com>
2017-01-06 15:49:15 -0500

    improve coverage

--- src/test/java/io/tchepannou/kiosk/pipeline/service/content/ContentExtractorIntegrationTest.java
+++ src/test/java/io/tchepannou/kiosk/pipeline/service/content/ContentExtractorIntegrationTest.java
@@ -1,8 +1,8 @@
 package io.tchepannou.kiosk.pipeline.service.content;
 
-import jdk.nashorn.internal.ir.annotations.Ignore;
 import org.apache.commons.io.IOUtils;
 import org.jsoup.Jsoup;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;

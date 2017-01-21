fd51d3c00af2128a56d4b9cd326a9e4ba3dbe449
htchepannou <htchepannou@expedia.com>
2017-01-21 17:29:25 -0500

    remove mondoblog

--- src/main/resources/db/migration/V1_25__onboard_mamymuna_mondoblog.sql
+++ src/main/resources/db/migration/V1_25__onboard_mamymuna_mondoblog.sql
@@ -1,11 +1,1 @@
 INSERT INTO feed (id, name, url, logo_url, path) VALUE (23, 'Mamy Muna', 'http://mamymuna.com', 'feeds/mamymuna.jpg', NULL);
-
-INSERT INTO feed (id, name, url, logo_url, path) VALUE (30, 'MondoBlog', 'http://mboattitudes.mondoblog.org', 'feeds/mondoblog.jpg', NULL);
-INSERT INTO feed (id, name, url, logo_url, path) VALUE (31, 'MondoBlog', 'http://monquotidien.mondoblog.org', 'feeds/mondoblog.jpg', NULL);
-INSERT INTO feed (id, name, url, logo_url, path) VALUE (32, 'MondoBlog', 'http://lepetitecolier.mondoblog.org', 'feeds/mondoblog.jpg', NULL);
-INSERT INTO feed (id, name, url, logo_url, path) VALUE (34, 'MondoBlog', 'http://enfantdafrica.mondoblog.org', 'feeds/mondoblog.jpg', NULL);
-INSERT INTO feed (id, name, url, logo_url, path) VALUE (35, 'MondoBlog', 'http://matango.mondoblog.org', 'feeds/mondoblog.jpg', NULL);
-INSERT INTO feed (id, name, url, logo_url, path) VALUE (36, 'MondoBlog', 'http://ntrjack.mondoblog.org', 'feeds/mondoblog.jpg', NULL);
-INSERT INTO feed (id, name, url, logo_url, path) VALUE (37, 'MondoBlog', 'http://dania.mondoblog.org', 'feeds/mondoblog.jpg', NULL);
-INSERT INTO feed (id, name, url, logo_url, path) VALUE (38, 'MondoBlog', 'http://ndengue.mondoblog.org', 'feeds/mondoblog.jpg', NULL);
-INSERT INTO feed (id, name, url, logo_url, path) VALUE (39, 'MondoBlog', 'http://achouka.mondoblog.org', 'feeds/mondoblog.jpg', NULL);

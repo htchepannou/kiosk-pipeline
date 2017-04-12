13f65ef207202cc99bf2fcfcfbcab95bf66d19e9
htchepannou <htchepannou@expedia.com>
2017-04-12 08:55:17 -0400

    update language when extracting content

--- src/main/java/io/tchepannou/kiosk/pipeline/step/content/ContentConsumer.java
+++ src/main/java/io/tchepannou/kiosk/pipeline/step/content/ContentConsumer.java
@@ -1,9 +1,12 @@
 package io.tchepannou.kiosk.pipeline.step.content;
 
+import io.tchepannou.kiosk.core.nlp.language.LanguageDetector;
 import io.tchepannou.kiosk.core.service.MessageQueue;
 import io.tchepannou.kiosk.pipeline.persistence.domain.Link;
 import io.tchepannou.kiosk.pipeline.step.AbstractLinkConsumer;
 import io.tchepannou.kiosk.pipeline.step.content.filter.ContentExtractor;
+import org.flywaydb.core.internal.util.StringUtils;
+import org.jsoup.Jsoup;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
@@ -21,8 +24,12 @@ public class ContentConsumer extends AbstractLinkConsumer {
     @Autowired
     ContentExtractor extractor;
 
+    @Autowired
+    LanguageDetector languageDetector;
+
     private String rawFolder;
     private String contentFolder;
+    private int defaultSummaryMaxLength;
 
     @Override
     protected void consume(final Link link) throws IOException {
@@ -55,9 +62,22 @@ public class ContentConsumer extends AbstractLinkConsumer {
     }
 
     private void updateLink(final Link link, final String key, final String html){
+        final String text = Jsoup.parse(html).text();
+
         link.setContentKey(key);
-        link.setContentLength(html.length());
+        link.setContentLength(text.length());
         link.setContentType("text/html");
+
+        if (link.getSummary() == null){
+            final String summary = StringUtils.left(text, defaultSummaryMaxLength);
+            link.setSummary(summary);
+        }
+
+        if (link.getLanguage() == null){
+            final String ltext = link.getTitle() + "\n" + link.getSummary();
+            link.setLanguage(languageDetector.detect(ltext));
+        }
+
         linkRepository.save(link);
     }
 
@@ -76,5 +96,13 @@ public class ContentConsumer extends AbstractLinkConsumer {
 
     public void setContentFolder(final String contentFolder) {
         this.contentFolder = contentFolder;
+    }
+
+    public int getDefaultSummaryMaxLength() {
+        return defaultSummaryMaxLength;
+    }
+
+    public void setDefaultSummaryMaxLength(final int defaultSummaryMaxLength) {
+        this.defaultSummaryMaxLength = defaultSummaryMaxLength;
     }
 }
--- src/test/java/io/tchepannou/kiosk/pipeline/step/content/ContentConsumerTest.java
+++ src/test/java/io/tchepannou/kiosk/pipeline/step/content/ContentConsumerTest.java
@@ -1,5 +1,6 @@
 package io.tchepannou.kiosk.pipeline.step.content;
 
+import io.tchepannou.kiosk.core.nlp.language.LanguageDetector;
 import io.tchepannou.kiosk.core.service.MessageQueue;
 import io.tchepannou.kiosk.pipeline.persistence.domain.Link;
 import io.tchepannou.kiosk.pipeline.step.LinkConsumerTestSupport;
@@ -18,6 +19,7 @@ import java.io.InputStream;
 import static io.tchepannou.kiosk.pipeline.Fixtures.readText;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.verify;
@@ -31,6 +33,9 @@ public class ContentConsumerTest extends LinkConsumerTestSupport {
     @Mock
     ContentExtractor extractor;
 
+    @Mock
+    LanguageDetector languageDetector;
+
     @InjectMocks
     ContentConsumer consumer;
 
@@ -38,6 +43,7 @@ public class ContentConsumerTest extends LinkConsumerTestSupport {
     public void setUp() {
         consumer.setRawFolder("html");
         consumer.setContentFolder("content");
+        consumer.setDefaultSummaryMaxLength(5);
     }
 
     @Test
@@ -45,6 +51,8 @@ public class ContentConsumerTest extends LinkConsumerTestSupport {
         // Given
         final Link link = new Link();
         link.setId(123);
+        link.setSummary("summary !!!");
+        link.setLanguage("en");
         link.setS3Key("html/2010/10/11/test.html");
 
         doAnswer(readText("hello world")).when(repository).read(any(), any());
@@ -69,5 +77,79 @@ public class ContentConsumerTest extends LinkConsumerTestSupport {
         assertThat(lk.getValue().getContentKey()).isEqualTo("content/2010/10/11/test.html");
         assertThat(lk.getValue().getContentLength()).isEqualTo(11);
         assertThat(lk.getValue().getContentType()).isEqualTo("text/html");
+        assertThat(lk.getValue().getSummary()).isEqualTo("summary !!!");
+        assertThat(lk.getValue().getLanguage()).isEqualToIgnoringCase("en");
+    }
+
+    @Test
+    public void shouldUpdateSummary() throws Exception {
+        // Given
+        final Link link = new Link();
+        link.setId(123);
+        link.setSummary(null);
+        link.setLanguage("en");
+        link.setS3Key("html/2010/10/11/test.html");
+
+        doAnswer(readText("hello world")).when(repository).read(any(), any());
+
+        when(extractor.extract("hello world")).thenReturn("HELLO WORLD");
+
+        // When
+        consumer.consume(link);
+
+        // Then
+        verify(messageQueue).push("123");
+
+        final ArgumentCaptor<InputStream> in = ArgumentCaptor.forClass(InputStream.class);
+        verify(repository).write(
+                eq("content/2010/10/11/test.html"),
+                in.capture()
+        );
+        assertThat(IOUtils.toString(in.getValue())).isEqualTo("HELLO WORLD");
+
+        final ArgumentCaptor<Link> lk = ArgumentCaptor.forClass(Link.class);
+        verify(linkRepository).save(lk.capture());
+        assertThat(lk.getValue().getContentKey()).isEqualTo("content/2010/10/11/test.html");
+        assertThat(lk.getValue().getContentLength()).isEqualTo(11);
+        assertThat(lk.getValue().getContentType()).isEqualTo("text/html");
+        assertThat(lk.getValue().getSummary()).isEqualTo("HELLO");
+        assertThat(lk.getValue().getLanguage()).isEqualToIgnoringCase("en");
+    }
+
+    @Test
+    public void shouldUpdateLanguage() throws Exception {
+        // Given
+        final Link link = new Link();
+        link.setId(123);
+        link.setSummary("summary !!!!");
+        link.setLanguage(null);
+        link.setS3Key("html/2010/10/11/test.html");
+
+        doAnswer(readText("hello world")).when(repository).read(any(), any());
+
+        when(extractor.extract("hello world")).thenReturn("HELLO WORLD");
+
+        when(languageDetector.detect(anyString())).thenReturn("ru");
+
+        // When
+        consumer.consume(link);
+
+        // Then
+        verify(messageQueue).push("123");
+
+        final ArgumentCaptor<InputStream> in = ArgumentCaptor.forClass(InputStream.class);
+        verify(repository).write(
+                eq("content/2010/10/11/test.html"),
+                in.capture()
+        );
+        assertThat(IOUtils.toString(in.getValue())).isEqualTo("HELLO WORLD");
+
+        final ArgumentCaptor<Link> lk = ArgumentCaptor.forClass(Link.class);
+        verify(linkRepository).save(lk.capture());
+        assertThat(lk.getValue().getContentKey()).isEqualTo("content/2010/10/11/test.html");
+        assertThat(lk.getValue().getContentLength()).isEqualTo(11);
+        assertThat(lk.getValue().getContentType()).isEqualTo("text/html");
+        assertThat(lk.getValue().getSummary()).isEqualTo("summary !!!!");
+        assertThat(lk.getValue().getLanguage()).isEqualToIgnoringCase("ru");
     }
 }
